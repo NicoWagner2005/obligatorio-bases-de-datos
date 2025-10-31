c88d70a8d3124bc42c7fec3f8da8cd9c07efc176
Guillermo Gonzalez <guillermo.gonzalezt@correo.ucu.edu.uy>
2025-10-31 14:00:02 -0300

    frontend v1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,22 @@ import './App.css'
 function App() {
   
   return (
-    <>
-      
-    </>
+      <div className="maincontainer">
+        <div className='ParteIzq'>
+            <h1 className='titulo'>Reserva de salones</h1>
+            <h2 className='subtitulo'>Sistema de reserva de salones para uso compartido, </h2>
+            <h2 className='subtitulo'>tanto curricular como extra curricular.</h2> 
+            
+        </div>
+        <div className="ParteDer">
+          <img src="../assets/images/logo-ucu-blanco.png" alt="logo UCU" className='logoUCU'/>
+          <div className='contenedorLogin'>
+              <input className='inputlogin' type="text" placeholder='usuario'/>
+              <input className='inputlogin' type="text" placeholder='contraseña'/>
+              <button className='botonsesion'>Iniciar sesión</button>
+          </div>
+        </div>
+      </div>
   )
 }
 
